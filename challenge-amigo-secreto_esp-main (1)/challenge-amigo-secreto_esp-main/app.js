18795ba12ae461ec90e79b76465e5b63e14ffd1b
jesusrodrig <rjesusdavid868@gmail.com>
2025-02-04 18:55:10 -0500

    Add files via upload

---
+++ challenge-amigo-secreto_esp-main (1)/challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,11 @@
+//Array para almacenar los nombres de amigos 
+let amigo =[];
+
+//Funciones para agregar un amigo
+function agregarAmigo(){
+    const imputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+
+    //Validar que el campo no este vacio
+    if(nombre =)
+}
